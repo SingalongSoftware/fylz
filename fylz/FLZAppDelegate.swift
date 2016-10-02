acaab7a48b36ec09cb3bd6914ef4fbdab9167c8d
asu <arsenyk.ustaris.dev@gmail.com>
2016-10-02 16:56:51 -0700

    Initial

---
+++ fylz/FLZAppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  fylz
+//
+//  Created by asu on 2016-10-02.
+//  Copyright © 2016 ArsenykUstaris. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class FLZAppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+
+
+}
+
---
+++ fylz/FLZViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fylz
+//
+//  Created by asu on 2016-10-02.
+//  Copyright © 2016 ArsenykUstaris. All rights reserved.
+//
+
+import UIKit
+
+class FLZViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
