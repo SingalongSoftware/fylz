4ab8e3503af2981a85d01c42a278aa7f62e4ac08
asu <arsenyk.ustaris.dev@gmail.com>
2016-10-02 18:46:38 -0700

    TableView setup.

--- fylz/ViewController/FLZViewController.swift
+++ fylz/ViewController/FLZViewController.swift
@@ -8,11 +8,18 @@
 
 import UIKit
 
-class FLZViewController: UIViewController {
+class FLZViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  @IBOutlet weak var tableView: UITableView!
+  
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +27,25 @@ class FLZViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  // MARK: UITableViewDataSource
 
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int // Default is 1 if not implemented
+  {
+    return 1
+  }
+  
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+  {
+    return 0
+  }
+  
+
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+  {
+    return UITableViewCell(frame: CGRectZero)
+  }
+  
+  
 }
 
