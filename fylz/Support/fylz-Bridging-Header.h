4edb31431f1ff45d331071a860e95eab519360b7
asu <arsenyk.ustaris.dev@gmail.com>
2016-10-03 21:48:06 -0700

    Experimental commit - use_frameworks! and Bridging Header both in use.

---
+++ fylz/Support/fylz-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  fylz-Bridging-Header.h
+//  fylz
+//
+//  Created by asu on 2016-10-02.
+//  Copyright © 2016 ArsenykUstaris. All rights reserved.
+//
+
+#ifndef fylz_Bridging_Header_h
+#define fylz_Bridging_Header_h
+
+
+#endif /* fylz_Bridging_Header_h */
